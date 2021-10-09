016e71c4dba25299bd60a71035a266ef745d8d72
fengerzhang <fengerzhang@tencent.com>
2021-10-09 15:35:05 +0800

    --增加 Pager包依赖 --修改 "登录"activity调整为compose --修改 更新kotlin和compose版本

--- app/src/main/java/com/fenger/wanandroid/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/fenger/wanandroid/ui/activity/LoginActivity.kt
@@ -3,12 +3,36 @@ package com.fenger.wanandroid.ui.activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.fenger.wanandroid.R
 import com.fenger.wanandroid.adapter.FragmentAdapter
 import com.fenger.wanandroid.base.BaseActivity
 import com.fenger.wanandroid.ui.fragment.LoginFragment
 import com.fenger.wanandroid.ui.fragment.RegisterFragment
-import kotlinx.android.synthetic.main.activity_login.login_viewpager
+import com.google.android.material.composethemeadapter.MdcTheme
 
 /**
  * @author fengerzhang
@@ -28,20 +52,61 @@ class LoginActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initView()
+        val composeView = findViewById<ComposeView>(R.id.login_compose_view)
+        composeView.setContent {
+            MdcTheme {
+                SetLoginInfo()
+            }
+        }
+    }
+
+    @Composable
+    fun SetLoginInfo() {
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.pager_image1),
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(top = 80.dp, bottom = 20.dp)
+                    .size(100.dp, 100.dp)
+            )
+            Text(
+                text = stringResource(id = R.string.welcome),
+                fontSize = 12.sp,
+                color = colorResource(id = R.color.white),
+            )
+            Text(
+                text = stringResource(id = R.string.app_fenger),
+                fontSize = 12.sp,
+                color = colorResource(id = R.color.white),
+                modifier = Modifier.alpha(0.8f).padding(top = 5.dp)
+            )
+
+        }
+    }
+
+    @Preview(showBackground = false)
+    @Composable
+    fun TestLogin() {
+        SetLoginInfo()
     }
 
     private fun initView() {
-        val fragments = listOf(LoginFragment(), RegisterFragment())
-        login_viewpager.adapter = FragmentAdapter(supportFragmentManager, fragments)
-        changeToLogin()
+//        val fragments = listOf(LoginFragment(), RegisterFragment())
+//        login_viewpager.adapter = FragmentAdapter(supportFragmentManager, fragments)
+//        changeToLogin()
     }
 
     fun changeToRegister() {
-        login_viewpager.currentItem = 1
+//        login_viewpager.currentItem = 1
     }
 
     fun changeToLogin() {
-        login_viewpager.currentItem = 0
+//        login_viewpager.currentItem = 0
     }
 }
